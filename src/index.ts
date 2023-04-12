82b8d721067c4451db04eceb140c409bec9d1f20
DasIschBims <dasischbims@gmail.com>
2023-04-13 01:56:11 +0200

    added bot activity

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import {ExtendedClient} from "./structs/client"
 import {Logger} from "./utils/logging/logger";
+import {setBotActivity} from "./utils/activity/setBotActivity";
+import {ActivityType} from "discord-api-types/v10";
+
 require("dotenv").config();
 
 export const client = new ExtendedClient();
 
 try {
-     client.start();
+     client.start().then(() => {
+         setBotActivity({text: "the source code.", type: ActivityType.Watching, status: "online"})
+     });
 } catch (e) {
     Logger.logError(`${e}`, "Startup")
 }
